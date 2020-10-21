142f363b9fd9e20b1fbb92053308af29456770d7
Precious Alex Kanu <kanuprecious89@gmail.com>
2020-10-21 07:14:40 +0100

    autoprefixer removed, plugin for purgecss added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [
-    './public/**/*.html',
-    './src/**/*.vue'
-  ],
   theme: {
     extend: {},
   },
